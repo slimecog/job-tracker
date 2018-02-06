dae44e5862171c917e5b130c2e71edf376449b47
slimecog <wlcjohnson@gmail.com>
2018-02-06 07:56:42 -0700

    fix category create test

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,7 @@ COMPANIES.each do |name|
   company = Company.create!(name: name)
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample)
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category_id: [1,2,3].sample)
     puts "  Created #{company.jobs[num].title}"
   end
 end
-
-Job.all.each do |job|
-  
--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 describe "user creates a new category" do
   scenario "a user can create a new category" do
+    category = Category.new(title: "Groceries")
     visit new_category_path
 
-    fill_in "category[title]", with: "Groceries"
+    fill_in "category[title]", with: "#{category.title}"
     click_button "Create Category"
 
-    expect(current_path).to eq(categories_path)
     expect(page).to have_content("Groceries")
-    expect(Category.count).to eq(4)
+    expect(Category.count).to eq(1)
   end
 end
