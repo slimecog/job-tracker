e711de2504754a0db6919e8b867d369fa19d63e7
slimecog <wlcjohnson@gmail.com>
2018-02-05 22:48:40 -0700

    add category to to seed file and update database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,15 @@ COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopi
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
 
+Category.create!(title: "Cosmetics")
+Category.create!(title: "Fashion")
+Category.create!(title: "Hygiene")
+
 COMPANIES.each do |name|
   company = Company.create!(name: name)
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample)
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category_id: [1,2,3].sample)
     puts "  Created #{company.jobs[num].title}"
   end
 end
