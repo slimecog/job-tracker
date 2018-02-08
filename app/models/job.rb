e8aaa58dd87e10aac7976d268680764423beddaa
slimecog <wlcjohnson@gmail.com>
2018-02-07 23:22:54 -0700

    add some style

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,6 @@
 class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :company
   belongs_to :category
-
-  def self.locations
-    group(:city).order("count(*) DESC").count
-  end
 end
--- db/migrate/20180206052719_add_category_id_to_jobs.rb
+++ db/migrate/20180206052719_add_category_id_to_jobs.rb
@@ -1,5 +1,6 @@
 class AddCategoryIdToJobs < ActiveRecord::Migration[5.1]
   def change
     add_reference :jobs, :category, index: true, foreign_key: true
+
   end
 end
